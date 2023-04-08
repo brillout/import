5fdd4d11a8b73e41d252c53006b9ae54eac7370d
Rob Koch <kochrt@gmail.com>
2023-04-08 13:57:34 -0400

    fix: use simplified regex for testing isAbsolute

--- index.ts
+++ index.ts
@@ -1,28 +1,9 @@
 export { import_ }
 export default import_
 
-function isPathSeparator(code: number) {
-  return code === 47 /* / */ || code === 92 /* \ */;
-}
-
-function isWindowsDeviceRoot(code: number) {
-  return (code >= 65 /* A */ && code <= 90 /* Z */) ||
-         (code >= 97 /* a */ && code <= 122 /* z */);
-}
-
-// https://github.com/nodejs/node/blob/49a77a5a996a49e8cb728eed42e55a7c1a9eef6e/lib/path.js#L402-L403
-function win32IsAbsolute(path: string) {
-  const len = path.length;
-  if (len === 0)
-    return false;
-
-  const code = path.charCodeAt(0);
-  return isPathSeparator(code) ||
-    // Possible device root
-    (len > 2 &&
-    isWindowsDeviceRoot(code) &&
-    path.charCodeAt(1) === 58 /* : */ &&
-    isPathSeparator(path.charCodeAt(2)));
+// https://github.com/unjs/pathe/blob/ae583c899ed9ebf44c94ab451da5fd7c3094dea9/src/path.ts#L197
+function isAbsolute(path: string) {
+ return /^[/\\](?![/\\])|^[/\\]{2}(?!\.)|^[A-Za-z]:[/\\]/.test(path)
 }
 
 function import_(id: string) {
@@ -37,7 +18,7 @@ function import_(id: string) {
 // https://stackoverflow.com/questions/69665780/error-err-unsupported-esm-url-scheme-only-file-and-data-urls-are-supported-by/70057245#70057245
 const prefix = 'file://'
 function fixWindowsBug(id: string): string {
-  if (process.platform === 'win32' && win32IsAbsolute(id) && !id.startsWith(prefix)) {
+  if (process.platform === 'win32' && isAbsolute(id) && !id.startsWith(prefix)) {
     return prefix + id
   } else {
     return id
