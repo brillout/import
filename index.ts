5d0ebc27c4641f1f91530428d286126a980bcf67
Romuald Brillout <git@brillout.com>
2023-06-04 09:32:36 +0200

    comment

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@ export default import_
 
 function import_(id: string) {
   id = fixWindowsBug(id)
+  // - For TypeScript to not transpile `import()` to `require()` for CJS builds, it needs to be configured with `"moduleResolution": "nodenext"`, see https://github.com/microsoft/TypeScript/issues/43329#issuecomment-1079559627
+  // - Skip webpack from forcefully bunlding dynamic imports with unknown IDs: https://github.com/webpack/webpack/issues/7644#issuecomment-402123392
   return import(/*webpackIgnore: true*/ id)
 }
 
